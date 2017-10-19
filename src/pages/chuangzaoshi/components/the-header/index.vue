56db428003cffffd01d406af0a8b1831c6d215a3
lsbbd <1206261187@qq.com>
2017-10-19 15:20:26 +0800

    分离 header 组件

---
+++ src/pages/chuangzaoshi/components/the-header/index.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <header>
+    <div class="button" :class="{active: sidebarActive}" @click="toggleSidebar()">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <div class="logo">
+      <img src="http://chuangzaoshi.com/assets/images/logo_mobile.svg" alt="">
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // 侧边栏可见性
+        sidebarActive: false
+      }
+    },
+
+    methods: {
+      toggleSidebar() {
+        this.sidebarActive = !this.sidebarActive
+        this.$parent.sidebarActive = !this.$parent.sidebarActive
+      }
+    },
+
+  }
+</script>
+
+<style lang="scss" src="./index.scss" scoped>
+</style>
--- src/pages/chuangzaoshi/index.vue
+++ src/pages/chuangzaoshi/index.vue
@@ -1,16 +1,6 @@
 <template lang="html">
   <div class="body">
-    <header>
-      <div class="button" :class="{active: sidebarActive}" @click="toggleSidebar()">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-
-      <div class="logo">
-        <img src="http://chuangzaoshi.com/assets/images/logo_mobile.svg" alt="">
-      </div>
-    </header>
+    <the-header></the-header>
 
     <aside class="sidebar" :class="{active: sidebarActive}">
       <the-sidebar></the-sidebar>
@@ -19,6 +9,7 @@
     <div class="main">
       <div class="wrapper">
         <toolbar></toolbar>
+
       </div>
     </div>
   </div>
@@ -28,22 +19,18 @@
   import Mock from 'mockjs'
   import theSidebar from './components/the-sidebar'
   import toolbar from './components/toolbar'
+  import theHeader from './components/the-header'
 
   export default {
     components: {
       theSidebar,
-      toolbar
+      toolbar,
+      theHeader
     },
 
     data() {
       return {
         sidebarActive: false
-      }
-    },
-
-    methods: {
-      toggleSidebar() {
-        this.sidebarActive = !this.sidebarActive
       }
     },
 
